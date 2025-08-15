341213afc96625e95f481d793f3fdb40a289658e
Mayank <mayankkumar0377+github@gmail.com>
2025-08-15 22:07:06 +0530

    backend setup complete

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDb = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
